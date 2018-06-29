3574222d1d54b8163a15ddff47c0786ef9f42651
Nikola Curilovic <ncurilovic@gmail.com>
2018-06-29 14:39:47 +0200

    The same thing we do every night, Pinky - try to take over the world.

---
+++ Domain/src/main/java/com/n1x0nj4/domain/model/Project.kt
@@ -0,0 +1,6 @@
+package com.n1x0nj4.domain.model
+
+class Project(val id: String, val name: String, val fullName: String,
+              val starCount: String, val dateCreated: String,
+              val ownerName: String, val ownerAvatar: String,
+              val isBookmarked: Boolean)
