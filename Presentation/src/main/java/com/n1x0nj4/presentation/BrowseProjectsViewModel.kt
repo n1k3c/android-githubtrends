e6981b014d0ba2b42dc5c0b8ac57a1de829d7f44
Nikola Curilovic <ncurilovic@gmail.com>
2018-07-05 14:01:09 +0200

    Creating the browse projects view model

---
+++ Presentation/src/main/java/com/n1x0nj4/presentation/BrowseProjectsViewModel.kt
@@ -0,0 +1,78 @@
+package com.n1x0nj4.presentation
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import com.n1x0nj4.domain.interactor.bookmark.BookmarkProject
+import com.n1x0nj4.domain.interactor.bookmark.UnbookmarkProject
+import com.n1x0nj4.domain.interactor.browse.GetProjects
+import com.n1x0nj4.domain.model.Project
+import com.n1x0nj4.presentation.mapper.ProjectViewMapper
+import com.n1x0nj4.presentation.model.ProjectView
+import com.n1x0nj4.presentation.state.Resource
+import com.n1x0nj4.presentation.state.ResourceState
+import io.reactivex.observers.DisposableCompletableObserver
+import io.reactivex.observers.DisposableObserver
+import javax.inject.Inject
+
+open class BrowseProjectsViewModel @Inject internal constructor(
+        private val getProjects: GetProjects?,
+        private val bookmarkProject: BookmarkProject,
+        private val unBookmarkProject: UnbookmarkProject,
+        private val mapper: ProjectViewMapper): ViewModel() {
+
+    private val liveData: MutableLiveData<Resource<List<ProjectView>>> = MutableLiveData()
+
+    init {
+        fetchProjects()
+    }
+
+    override fun onCleared() {
+        getProjects?.dispose()
+        super.onCleared()
+    }
+
+    fun getProjects(): LiveData<Resource<List<ProjectView>>> {
+        return liveData
+    }
+
+    fun fetchProjects() {
+        liveData.postValue(Resource(ResourceState.LOADING, null, null))
+        getProjects?.execute(ProjectsSubscriber())
+    }
+
+    fun bookmarkProject(projectId: String) {
+        return bookmarkProject.execute(BookmarkProjectsSubscriber(),
+                BookmarkProject.Params.forProject(projectId))
+    }
+
+    fun unbookmarkProject(projectId: String) {
+        return unBookmarkProject.execute(BookmarkProjectsSubscriber(),
+                UnbookmarkProject.Params.forProject(projectId))
+    }
+
+    inner class ProjectsSubscriber: DisposableObserver<List<Project>>() {
+        override fun onNext(t: List<Project>) {
+            liveData.postValue(Resource(ResourceState.SUCCESS,
+                    t.map { mapper.mapToView(it) }, null))
+        }
+
+        override fun onComplete() { }
+
+        override fun onError(e: Throwable) {
+            liveData.postValue(Resource(ResourceState.ERROR, null, e.localizedMessage))
+        }
+
+    }
+
+    inner class BookmarkProjectsSubscriber: DisposableCompletableObserver() {
+        override fun onComplete() {
+            liveData.postValue(Resource(ResourceState.SUCCESS, liveData.value?.data, null))
+        }
+
+        override fun onError(e: Throwable) {
+            liveData.postValue(Resource(ResourceState.ERROR, liveData.value?.data,
+                    e.localizedMessage))
+        }
+    }
+}
